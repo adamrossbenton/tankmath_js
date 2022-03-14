81757648ab6a60e17503e53c2344ee9c042d3091
adamrossbenton <adamrossbenton@gmail.com>
2022-03-13 21:29:57 -0700

    setting up api info

---
+++ src/hooks/api.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function api() {
+
+    // COUNTRIES AND CURRENCIES API
+    const countryAPI = "https://restcountries.com/v3.1/all"
+
+    // CURRENCY CONVERSION API
+    const converterAPI = ""
+
+}
+
+export default api

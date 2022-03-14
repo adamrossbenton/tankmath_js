f4c1ddc4e91f13be499422b7f48caafa746c09dc
adamrossbenton <adamrossbenton@gmail.com>
2022-03-13 21:43:14 -0700

    more api setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// DEPENDENCIES
+
+// API
+import api from "./hooks/api"
+// STYLES
 import './App.css';
 
+
 function App() {
+  
+  const {countryAPI, converterAPI} = api()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello app</h1>
+      <h3>country api: {countryAPI}</h3>
+      <h3>converter api: {converterAPI}</h3>
     </div>
   );
 }
--- src/hooks/api.js
+++ src/hooks/api.js
@@ -2,11 +2,18 @@ import React from "react"
 
 function api() {
 
+    const KEY=process.env.REACT_APP_KEY
+
     // COUNTRIES AND CURRENCIES API
     const countryAPI = "https://restcountries.com/v3.1/all"
 
     // CURRENCY CONVERSION API
-    const converterAPI = ""
+    const converterAPI = `http://api.exchangeratesapi.io/v1/latest?access_key=${KEY}`
+
+    return {
+        countryAPI,
+        converterAPI,
+    }
 
 }
 
